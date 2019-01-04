f09d358224e2f98ae48714c44d98c2b8287d9e2a
Nguyen Duc Tuan Anh <nhongbay2112bt@gmail.com>
2019-01-04 14:29:28 +0700

    022904012019

---
+++ wwwroot/js/FilesUpload.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $("#MeetingTitle").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: document.location.origin + "/FilesUpload/CreateData",
+                type: "POST",
+                dataType: "json",
+                data: { Prefix: request.term },
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return { value: item.id, label: item.meetingTitle }
+                    }));
+                }
+            });
+        },
+        select: function (event, ui) {
+            event.preventDefault();
+            $("#MeetingTitle").val(ui.item.label);
+            $("#MeetingID").val(ui.item.value);
+            $("#FileUpload_MeetingID").val(ui.item.value);
+        },
+        focus: function (event, ui) {
+            event.preventDefault();
+            $("#MeetingTitle").val(ui.item.label);
+            $("#MeetingID").val(ui.item.value);
+            $("#FileUpload_MeetingID").val(ui.item.value);
+        }
+    });
+    $("#MeetingTitle").on('change', function () {
+        if ($(this).val() == "") {
+            $("#MeetingID").val("");
+            $("#FileUpload_MeetingID").val("");
+        }
+    });
+    $.validator.setDefaults({ ignore: null });
+});
---
+++ wwwroot/js/TransactionUser.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#TransactionUserName").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: document.location.origin + "/Funds/CreateData",
+                type: "POST",
+                dataType: "json",
+                data: { Prefix: request.term },
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return { value: item.id, label: item.name }
+                    }));
+                }
+            });
+        },
+        select: function (event, ui) {
+            event.preventDefault();
+            $("#TransactionUserName").val(ui.item.label);
+            $("#zfunds_TransactionUserID").val(ui.item.value);
+        },
+        focus: function (event, ui) {
+            event.preventDefault();
+            $("#TransactionUserName").val(ui.item.label);
+        }
+    });
+    $.validator.setDefaults({ ignore: null });
+});
